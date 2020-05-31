f0531654524dbbfe174cdc1c414bb63de7e76415
Maksim Ermolenkov <zilatov@gmail.com>
2020-05-31 20:20:30 +0300

    Урок 4 - обязательная часть

---
+++ lesson4/flight-control-system/fcs.js
@@ -0,0 +1,304 @@
+function makeTime(hours, minutes) {
+    const date = new Date();
+    date.setHours(hours);
+    date.setMinutes(minutes);
+    date.setSeconds(0);
+    date.setMilliseconds(0);
+    return date.getTime();
+}
+
+/**
+ * @type {Object<string, Flight>} Список всех рейсов
+ */
+let flights = {
+    BH118: {
+        name: 'BH118',
+        seats: 28,
+        businessSeats: 4,
+        registrationStarts: makeTime(10, 0),
+        registartionEnds: makeTime(15, 0),
+        tickets: [
+            {
+                id: 'BH118-B50',
+                flight: 'BH118',
+                fullName: 'Ivanov I. I.',
+                type: 0,
+                seat: 18,
+                buyTime: makeTime(2, 0),
+                registrationTime: null,
+            }
+        ],
+    }
+};
+
+/**
+ * Добавление рейса
+ *
+ * * назначение номера рейса
+ * * подготовка рейса
+ *   * вычисление времени регистрации
+ *   * подготовка структуры Flight
+ *
+ * @param {Airliner} airliner Информация о самолете
+ * @param {number} time Время вылета
+ * @returns {Flight}
+ */
+// function createFlight(airliner, time) { }
+
+/**
+ * Поиск свободного места нужного типа
+ *
+ * Гарантирует что найдет свободное место нужного типа или вернет null
+ *
+ * @param {Flight} flight
+ * @param {number} type
+ * @returns {number} seat
+ */
+function findAvailableSeat(flight, type) {
+    let exists;
+    let seat;
+    let seatsOfType = 0;
+
+    switch (type) {
+        case 0: // standart
+            const availableSeats = [];
+
+            for (let i = flight.businessSeats + 1; i <= flight.seats; i++)
+                if (!flight.tickets.find(item => item.seat === i))
+                    availableSeats.push(i)
+
+            if (availableSeats.length === 0)
+                return null;
+
+            const index = Math.floor(Math.random() * availableSeats.length);
+            return availableSeats[index];
+        case 1: // business
+            for (let i = 1; i <= flight.businessSeats; i++)
+                if (!flight.tickets.find(item => item.seat === i))
+                    seatsOfType++;
+
+            if (seatsOfType === 0)
+                return null;
+
+            do {
+                seat = Math.floor(Math.random() * flight.businessSeats) + 1;
+                exists = flight.tickets.find(item => item.seat === seat);
+            } while (exists);
+
+            return seat;
+        default:
+            throw new Error(`Unknown type`)
+    }
+}
+
+/**
+ * Покупка билета на самолет
+ *
+ * * проверка рейса
+ * * проверка возможности купить (время и наличие мест)
+ * * сохранение данных билета в информации о рейсе
+ *
+ * @param {string} flightName Номер рейса
+ * @param {number} buyTime Время покупки
+ * @param {string} fullName Имя пассажира
+ * @param {number} type Тип места
+ * @returns {Ticket} Возвращаем копию билета
+ */
+function buyTicket(flightName, buyTime, fullName, type = 0) {
+    const flight = flights[flightName];
+
+    if (!flight)
+        throw new Error('Flight not found');
+
+    if (flight.tickets.length >= flight.seats)
+        throw new Error('No seats available');
+
+    if (buyTime > flight.registartionEnds)
+        throw new Error('Time away');
+
+    const seat = findAvailableSeat(flight, type);
+    if (!seat)
+        throw new Error(`No seats of type ${type} available. You can choose another type`);
+
+    let id;
+    do {
+        id = flight.name + '-' + Math.random().toString().substr(2, 3);
+        exists = flight.tickets.find(item => item.id === id);
+    } while (exists);
+
+    /**
+     * @type {Ticket}
+     */
+    const ticket = {
+        id,
+        flight: flight.name,
+        buyTime,
+        fullName,
+        registrationTime: null,
+        type,
+        seat,
+    }
+
+    flight.tickets.push(ticket);
+
+    // return Object.assign({}, ticket);
+    return {
+        ...ticket,
+        welcome: 'Nice to choose us',
+    };
+}
+
+const a = buyTicket('BH118', makeTime(5, 10), 'Petrov I. I.');
+console.log(a);
+
+
+function displayFlights() {
+    console.log('*** List of all flights ***');
+    console.table(flights);
+}
+
+function flightDetails(flightName) {
+    console.log(`*** Details of flight ${flightName} ***`);
+    const flight = flights[flightName];
+    if (!flight) {
+        console.warn('Flight not found');
+        return;
+    }
+
+    console.table(flight);
+    console.table(flight.tickets);
+}
+
+/**
+ * Функция пробует произвести электронную регистрацию пассажира
+ *
+ *  * проверка билета
+ *  * проверка данных пассажира
+ *  * электронную регистрацию можно произвести только в период от 5 до 1 часа до полета
+ *
+ * @param {string} ticket номер билета
+ * @param {string} fullName имя пассажира
+ * @param {number} nowTime текущее время
+ * @returns boolean успешна ли регистрация
+ */
+function eRegistration(ticket, fullName, nowTime) {
+    let flightName = ticket.split('-')[0];
+    let flight = flights[flightName];
+
+    let clientTicket = flight.tickets.find(function(purchasedTicket) {
+            if (purchasedTicket.id === ticket && purchasedTicket.fullName === fullName) {
+                return purchasedTicket;
+            };
+    })
+
+    if (!clientTicket) {
+        console.warn('Ticket not found');
+        return false;
+    }
+
+    if (flight.registrationStarts < nowTime && flight.registartionEnds > nowTime) {
+        clientTicket.registrationTime = nowTime;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/**
+ * Отчет о рейсе на данный момент
+ *
+ * @typedef {Object} Report
+ * @property {string} flight Номер рейса
+ * @property {boolean} registration Доступна регистрация на самолет
+ * @property {boolean} complete Регистрация завершена или самолет улетел
+ * @property {number} countOfSeats Общее количество мест
+ * @property {number} reservedSeats Количество купленных (забронированных) мест
+ * @property {number} registeredSeats Количество пассажиров, прошедших регистрацию
+ */
+
+ /**
+ * Функция генерации отчета по рейсу
+ *
+ *  * проверка рейса
+ *  * подсчет
+ *
+ * @param {string} flight номер рейса
+ * @param {number} nowTime текущее время
+ * @returns {Report} отчет
+ */
+function flightReport(flight, nowTime) {
+    let flightObject = flights[flight];
+
+    if (!flight) {
+        console.warn('Flight not found');
+        return false;
+    }
+
+    let registration = (flightObject.registrationStarts < nowTime && flightObject.registartionEnds > nowTime);
+    let complite = nowTime > flightObject.registartionEnds;
+    let countOfSeats = flightObject.seats;
+    let reservedSeats = flightObject.tickets.length;
+    let registeredSeats = 0;
+
+
+    flightObject.tickets.forEach((ticket) => {
+        if (ticket.registrationTime !== null) {
+            ++registeredSeats;
+        }
+    })
+
+    let report = {
+        'flight': flight,
+        'registration': registration,
+        'complete': complite,
+        'countOfSeats': countOfSeats,
+        'reservedSeats': reservedSeats,
+        'registeredSeats': registeredSeats
+    };
+
+    return report;
+}
+
+function flightDetails(flightName) {
+    let flight = flights[flightName];
+    flightTemplate(flight);
+    
+    if (flight.tickets) {
+        ticketsTemplate(flight.tickets);
+    }
+}
+
+function ticketsTemplate(tickets) {         
+    const ticketsDiv = document.getElementById('tickets');       
+    tickets.forEach(function(ticket) { 
+        let ul = document.createElement('ul');  
+        let ticketFields = [
+            `number: ${ticket.id}`,
+            `seat: ${ticket.seat}`,
+            `full name: ${ticket.fullName}`,
+            `registartion: ${ticket.registartionTime !== null ? true : false}`
+        ]   
+
+        ticketFields.map(function(string) {
+            let li = document.createElement('li');
+            text = document.createTextNode(string)
+            li.append(text);
+            ul.append(li)        
+        })
+        ticketsDiv.append(ul)  
+    })    
+}
+
+function flightTemplate(flight) {
+    const flightDetailsDiv = document.getElementById('flight-details');
+    let ul = document.createElement('ul');
+    for (let [key, value] of Object.entries(flight)) {
+        if (key !== 'tickets') {
+            let li = document.createElement('li');
+            let text = document.createTextNode(key + ' : ' + value);
+            li.append(text);
+            ul.append(li);
+        }
+    }   
+    flightDetailsDiv.append(ul); 
+}
